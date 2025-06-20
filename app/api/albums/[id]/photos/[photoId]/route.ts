55dc3243b2f6ea37dbed68eaa26176dde29e0166
Kadir Yetim <mr.kyetim12@gmail.com>
2025-06-20 15:17:27 +0300

    Prima importları 2. kez güncellendi

--- app/api/albums/[id]/photos/[photoId]/route.ts
+++ app/api/albums/[id]/photos/[photoId]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import prisma from '../../../../../lib/prisma'
+import { prisma } from '../../../../../lib/prisma'
 import { unlink } from 'fs/promises'
 import { join } from 'path'
 
